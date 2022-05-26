ebea2f8b056e161174c7ab9e3ec2098b2cb3d23b
UNASP-HT - Vinicius Guimarães Vieira <vinicius.21666@unasp-ht.edu.br>
2022-05-26 12:32:59 -0300

    SalvaProva26/05
    Saveeeee

--- loginv2.php
+++ loginv2.php
@@ -1,3 +1,24 @@
+<?php
+
+include'connect.php';
+
+if(isset($_POST['sub'])){
+    $u=$_POST['user'];
+    $p=$_POST['pass'];
+    $s= "select * from reg where username='$u' and password= '$p'";   
+   $qu= mysqli_query($con, $s);
+   if(mysqli_num_rows($qu)>0){
+      $f= mysqli_fetch_assoc($qu);
+      $_SESSION['id']=$f['id'];
+      header ('location:AdminLTE-3.2.0/index.html');
+   }
+   else{
+       echo 'username or password does not exist';
+   }
+  
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,16 +38,16 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE - 3TI</a>
+    <a href="AdminLTE-3.2.0/index2.html"><b>Admin</b>LTE - 3TI</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="../../index3.html" method="post">
+      <form method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="Text" class="form-control" placeholder="Username" name="user">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -34,7 +55,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="Password" name="pass">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -52,7 +73,7 @@
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block" name="sub" >Sign In</button>
           </div>
           <!-- /.col -->
         </div>
@@ -82,10 +103,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
 </body>
 </html>
